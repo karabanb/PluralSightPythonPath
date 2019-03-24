6a51f4bc2954c8a0a2129e5e89077ea709dd0054
karabanb <b.karaban@me.com>
2019-03-24 19:46:23 +0100

    format function with examples

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,11 @@
 
 print("Hello World!")
+
+# string format function
+
+name = 'Bartek'
+machine = 'Mac'
+
+print("Hello {0} !. I`m {1}".format(name, machine))
+
+print(f"Nice to meet you {name}, I`m {machine} :)")
