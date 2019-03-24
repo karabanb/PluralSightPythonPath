10d5085253bb197a888fb0e3d1691bc8ceb2ce9b
karabanb <b.karaban@me.com>
2019-03-24 23:25:57 +0100

    format function with examples

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
+from typing import List
 
 print("Hello World!")
 
@@ -9,3 +10,25 @@ machine = 'Mac'
 print("Hello {0} !. I`m {1}".format(name, machine))
 
 print(f"Nice to meet you {name}, I`m {machine} :)")
+
+''' ########## statements ########## '''
+
+if name.isdigit():
+    print("No!")
+else:
+    print("Yes")
+
+''' ############ lists ############# '''
+
+students = ['Anna', 'Maria', 'Wesolowska']
+
+print(students)
+print(students[0] == 'Anna')
+
+students[2] = 'Jola'  # replacing elements
+print(students)
+
+students.append("Tola")  # adding elements
+print(students)
+
+len(students)  # number of elements
