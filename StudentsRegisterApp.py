259817d24aafcb1acaa34169946d4bc0ce02215e
karabanb <b.karaban@me.com>
2019-03-26 17:55:44 +0100

    code after function module form plrsght

---
+++ StudentsRegisterApp.py
@@ -0,0 +1,22 @@
+
+students = ['jan']
+
+
+def get_student_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase
+
+
+def print_student_titlecase():
+    students_titlecase = get_student_titlecase()
+    print(students_titlecase)
+
+
+def add_student(name):
+    students.append(name)
+
+
+student_list = get_student_titlecase()
+
